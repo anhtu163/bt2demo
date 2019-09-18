0b949b233f03fc5baaf5573506defa5c56a1a56b
Anh Từ <44427982+anhtu163@users.noreply.github.com>
2019-09-18 12:11:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,10 +17,10 @@ function Square(props) {
 function calculateWinner(squares) {
   for (var i = 0; i < squares.length; i++) {
     if (squares[i]) {
-      if (duyetDoc(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
-        duyetNgang(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
-        duyetCheoXuoi(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
-        duyetCheoNguoc(parseInt(i / COL, 10), parseInt(i % COL, 10), squares)
+      if (checkCol(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
+        checkRow(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
+        checkDiagonal1(parseInt(i / COL, 10), parseInt(i % COL, 10), squares) ||
+        checkDiagonal2(parseInt(i / COL, 10), parseInt(i % COL, 10), squares)
       ) {
         return true;
       }
@@ -29,46 +29,46 @@ function calculateWinner(squares) {
   return false;
 }
 
-function duyetDoc(dong, cot, arr) {
-  if (dong > ROW - 5) {
+function checkCol(row, col, arr) {
+  if (row > ROW - 5) {
     return false;
   }
-  let dem;
+  let count;
 
-  for (dem = 1; dem < 5; dem++) {
-    if (arr[(dong + dem) * COL + cot] !== arr[dong * COL + cot]) {
+  for (count = 1; count < 5; count++) {
+    if (arr[(row + count) * COL + col] !== arr[row * COL + col]) {
       return false;
     }
   }
   //chặn bởi biên
-  if (dong === 0 || (dong + dem) === COL) {
+  if (row === 0 || (row + count) === COL) {
     return true;
   }
   //không chặn 2 đầu
-  if (arr[(dong - 1) * COL + cot] === null || arr[(dong + dem) * COL + cot] === null) {
+  if (arr[(row - 1) * COL + col] === null || arr[(row + count) * COL + col] === null) {
     return true;
   }
   return false;
 }
 
-function duyetNgang(dong, cot, arr) {
-  if (cot > COL - 5) {
+function checkRow(row, col, arr) {
+  if (col > COL - 5) {
     return false;
   }
-  let dem;
+  let count;
 
-  for (dem = 1; dem < 5; dem++) {
-    if (arr[dong * COL + (cot + dem)] !== arr[dong * COL + cot]) {
+  for (count = 1; count < 5; count++) {
+    if (arr[row * COL + (col + count)] !== arr[row * COL + col]) {
       return false;
     }
 
   }
   //chặn bởi biên
-  if (cot === 0 || (cot + dem) === COL) {
+  if (col === 0 || (col + count) === COL) {
     return true;
   }
   //không chặn 2 đầu
-  if (arr[dong * COL + (cot - 1)] === null || arr[dong * COL + (cot + dem)] === null) {
+  if (arr[row * COL + (col - 1)] === null || arr[row * COL + (col + count)] === null) {
     return true;
   }
   return false;
@@ -76,43 +76,43 @@ function duyetNgang(dong, cot, arr) {
 
 
 // cheo \
-function duyetCheoXuoi(dong, cot, arr) {
-  if (dong > COL - 5 || cot > COL - 5) {
+function checkDiagonal1(row, col, arr) {
+  if (row > COL - 5 || col > COL - 5) {
     return false;
   }
-  let dem;
+  let count;
 
-  for (dem = 1; dem < 5; dem++) {
-    if (arr[(dong + dem) * COL + (cot + dem)] !== arr[dong * COL + cot]) {
+  for (count = 1; count < 5; count++) {
+    if (arr[(row + count) * COL + (col + count)] !== arr[row * COL + col]) {
       return false;
     }
 
   }
-  if (dong === 0 || (dong + dem) === ROW || cot === 0 || (cot + dem) === COL) {
+  if (row === 0 || (row + count) === ROW || col === 0 || (col + count) === COL) {
     return true;
   }
-  if (arr[(dong - 1) * COL + (cot - 1)] === null || arr[(dong + dem) * COL + (cot + dem)] === null) {
+  if (arr[(row - 1) * COL + (col - 1)] === null || arr[(row + count) * COL + (col + count)] === null) {
     return true;
   }
   return false;
 }
 
 // cheo /
-function duyetCheoNguoc(dong, cot, arr) {
-  if (dong < 4 || cot > COL - 5) {
+function checkDiagonal2(row, col, arr) {
+  if (row < 4 || col > COL - 5) {
     return false;
   }
-  let dem;
+  let count;
 
-  for (dem = 1; dem < 5; dem++) {
-    if (arr[(dong - dem) * COL + (cot + dem)] !== arr[dong * COL + cot]) {
+  for (count = 1; count < 5; count++) {
+    if (arr[(row - count) * COL + (col + count)] !== arr[row * COL + col]) {
       return false;
     }
   }
-  if (dong === 4 || dong === (ROW - 1) || cot === 0 || (cot + dem) === COL) {
+  if (row === 4 || row === (ROW - 1) || col === 0 || (col + count) === COL) {
     return true;
   }
-  if (arr[(dong + 1) * COL + (cot - 1)] === null || arr[(dong - dem) * COL + (cot + dem)] === null) {
+  if (arr[(row + 1) * COL + (col - 1)] === null || arr[(row - count) * COL + (col + count)] === null) {
     return true;
   }
   return false;
@@ -150,7 +150,9 @@ class Board extends Component {
     });
     if (squares[i] && calculateWinner(squares)) {
       console.log('winner');
+
       this.setState({ status: true, });
+
       return;
     }
 
@@ -184,15 +186,18 @@ class Board extends Component {
     let list = numbers.map(number => (
       this.renderSquare(number)
     ));
+
     return (
       <div>
         <div className="status">{status}</div>
+
         <div>
           <button onClick={() => this.newGame()}>New Game</button>
         </div>
         <div className="board-row" >
           {list}
         </div>
+       
       </div>
 
     )
